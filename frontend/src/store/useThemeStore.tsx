82e2245d5885009320ba914717e97cabc190c143
Dawid Garncarz <dawid.garncarz@mytracko.pl>
2025-12-10 16:29:45 +0100

    removed comment

--- frontend/src/store/useThemeStore.tsx
+++ frontend/src/store/useThemeStore.tsx
@@ -9,16 +9,6 @@ interface ThemeStore {
 	setTheme: (theme: ThemeName) => void;
 }
 
-// export const useThemeStore = create<ThemeStore>(set => ({
-
-// 	theme: localStorage.getItem('preferred-theme') || (mq.matches ? 'night' : 'pastel'),
-// 	themeColors: JSON.parse(localStorage.getItem('preferred-theme-colors') || JSON.stringify(mq.matches ? ["#0F172A", "#334155", "#64748B"] : ["#ffd8d8", "#b7e4c7", "#d8b4fe"])),
-// 	setTheme: (theme: string) => {
-// 		localStorage.setItem('preferred-theme', theme);
-// 		set({ theme });
-// 	},
-// }));
-
 export const useThemeStore = create<ThemeStore>(set => {
 	const savedTheme = localStorage.getItem('preferred-theme') as ThemeName | null;
 	const savedColors = localStorage.getItem('preferred-theme-colors');
